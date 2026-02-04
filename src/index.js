ff9375c89c8d49aea17226be23ace2d0f1052fe9
vitinhozip <costasousavitorgabriel@gmail.com>
2026-02-04 04:53:10 -0300

    adicionando a segunda função e finalizando o projeto

--- src/index.js
+++ src/index.js
@@ -1,6 +1,30 @@
-let resultado = saldo(5, 3)
-console.log(resultado)
+//variáveis
+let saldoVitorias = saldo(33, 33)
+let ranked = saldoVitorias
+let final = nivel(ranked)
 
+//funções
 function saldo(vitorias, derrotas){
     return vitorias - derrotas
 }
+
+function nivel(ranked){
+    if(ranked<=10){
+        return 'Ferro'
+    }else if(ranked>=11 && ranked<=20){
+        return 'Bronze'
+    }else if(ranked>=21 && ranked<=50){
+        return 'Prata'
+    }else if(ranked>=51 && ranked<=80){
+        return 'Ouro'
+    }else if(ranked>=81 && ranked<=90){
+        return 'Diamante'
+    }else if(ranked>=91 && ranked<=100){
+        return 'Lendário'
+    }else if(ranked>= 101){
+        return 'Imortal'
+    }
+    
+}
+
+console.log('O herói tem um saldo de ' + saldoVitorias + ' e está no nível ' + final)
